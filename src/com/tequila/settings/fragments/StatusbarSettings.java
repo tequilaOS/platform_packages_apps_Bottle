fb08f3aa26d6d6e02da4cbe7fbab53b2ddca7144
Ido Ben-Hur <idoybh2@gmail.com>
2022-11-23 05:46:30 +0000

    tequilaSettings: Add back all SB network traffic options this is a rewrite and a squash of many commits, giving co-authorship to relevant contributors.
    My edits:
    * Made it a master switch
    * Adjusted Strings
    * Improved code around
    
    https://github.com/yaap/packages_apps_YASP/commit/bc509c5b55ff84c2a5704bd2be29ed9b2ab5976a
    
    Co-authored-by: BoredOutOfMyGit <du.alexcruz@gmail.com>
    Co-authored-by: ezio84 <brabus84@gmail.com>
    Co-authored-by: Nico60 <ndhouailly@gmail.com>
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
    Co-authored-by: calebcabob <calphonic@gmail.com>
    Co-authored-by: AnkitGourav <Ankitgourav035@gmail.com>
    
    tequilaSettings : NetworkTraffic: Actually scale in DP for single net font [2/2]
    Lying to the user is bad
    https://github.com/yaap/packages_apps_YASP/commit/e9bd8d95976ac9dfdb86166e9b085092f276b5fc
    
    tequilaSettings : Dynamic summary for network traffic monitor
    https://github.com/yaap/packages_apps_YASP/commit/317d391468eae463461e3de5df5f6daf851baf72
    
    tequilaSettings: Fixup default values in net traffic settings
    https: //github.com/yaap/packages_apps_YASP/commit/2be397874013c0faa1936441c715f3773f3e1c49
    Change-Id: Ibe86647c4eab84cbe163e5e8ab38f38ca0d2b1f0

--- src/com/tequila/settings/fragments/StatusbarSettings.java
+++ src/com/tequila/settings/fragments/StatusbarSettings.java
@@ -1,32 +1,127 @@
 package com.tequila.settings.fragments;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.text.format.DateFormat;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 
 import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
-
+import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.search.Indexable;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.tequila.support.preferences.SystemSettingMasterSwitchPreference;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 public class StatusbarSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+        OnPreferenceChangeListener, Indexable {
+
+    private static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
+    private SystemSettingMasterSwitchPreference mNetTrafficState;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.tequila_settings_statusbar);
+
+	 PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mNetTrafficState = (SystemSettingMasterSwitchPreference)
+                findPreference(NETWORK_TRAFFIC_STATE);
+        mNetTrafficState.setOnPreferenceChangeListener(this);
+        boolean enabled = Settings.System.getInt(resolver,
+                NETWORK_TRAFFIC_STATE, 0) == 1;
+        mNetTrafficState.setChecked(enabled);
+        updateNetTrafficSummary(enabled);
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object value) {
+    public void onResume() {
+        super.onResume();
+        updateNetTrafficSummary();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mNetTrafficState) {
+            boolean enabled = (boolean) objValue;
+            Settings.System.putInt(resolver, NETWORK_TRAFFIC_STATE, enabled ? 1 : 0);
+            updateNetTrafficSummary(enabled);
+            return true;
+        }
         return false;
+    }
+
+    private void updateNetTrafficSummary() {
+        final boolean enabled = Settings.System.getInt(
+                getActivity().getContentResolver(),
+                NETWORK_TRAFFIC_STATE, 0) == 1;
+        updateNetTrafficSummary(enabled);
+    }
+
+    private void updateNetTrafficSummary(boolean enabled) {
+        if (mNetTrafficState == null) return;
+        String summary = getActivity().getString(R.string.switch_off_text);
+        if (enabled) {
+            final boolean onStatus = Settings.System.getInt(
+                    getActivity().getContentResolver(),
+                    Settings.System.NETWORK_TRAFFIC_VIEW_LOCATION, 0) == 0;
+            summary = getActivity().getString(R.string.network_traffic_state_summary);
+            summary += " " + (onStatus
+                    ? getActivity().getString(R.string.traffic_statusbar)
+                    : getActivity().getString(R.string.traffic_expanded_statusbar));
+        }
+        mNetTrafficState.setSummary(summary);
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.TEQUILA;
     }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                                                                            boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.tequila_settings_statusbar;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+            };
 }
