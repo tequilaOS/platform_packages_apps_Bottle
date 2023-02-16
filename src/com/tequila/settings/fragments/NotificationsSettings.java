4b7948faf9d668bca3c5cb9d627135a66883ffd3
beanstown106 <nbenis106@gmail.com>
2023-02-16 17:41:28 +0800

    tequilaSettings: incall vibration options [3/3]
    *allow setting vibration when call is connected
    *allow setting vibration when call is disconnected
    *allow setting vibration for call waiting
    *this works with google and aosp dialer :)
    
    Change-Id: Id1a431124174571f8d0091825af2418971cfe35d
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/com/tequila/settings/fragments/NotificationsSettings.java
+++ src/com/tequila/settings/fragments/NotificationsSettings.java
@@ -12,6 +12,7 @@ import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragment;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.tequila.TequilaUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -19,10 +20,20 @@ import com.android.settings.SettingsPreferenceFragment;
 public class NotificationsSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.tequila_settings_notifications);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefSet = getPreferenceScreen();
+
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!TequilaUtils.isVoiceCapable(getActivity())) {
+                prefSet.removePreference(incallVibCategory);
+        }
     }
 
     @Override
