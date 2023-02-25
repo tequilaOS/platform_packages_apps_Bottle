cf366bd526b557cbd67b8182cbabeede8086cd1c
x0x00044 <x0x00044@x0x00044.me>
2023-02-25 19:28:21 +0100

    tequilaSettings: Create QuickSettings fragment
    Moved settings from the following changes:
    Id5f0f4b0eda37dad5563a360f65e397e599b5bbd
    I02fa8091e8b38c861315d685d4cd89c75a173d47
    
    Change-Id: I412bbdeb081fe91ce96faf7ca996442604f90828
    Signed-off-by: x0x00044 <x0x00044@x0x00044.me>

--- src/com/tequila/settings/tequilaSettings.java
+++ src/com/tequila/settings/tequilaSettings.java
@@ -18,7 +18,8 @@ public class tequilaSettings extends SettingsPreferenceFragment {
 
         findPreference("about_fragment").setLayoutResource(R.layout.tequila_settings_layout);
         findPreference("statusbar_fragment").setLayoutResource(R.layout.top_level_preference_top);
-        findPreference("lockscreen_fragment").setLayoutResource(R.layout.top_level_preference_bottom);
+        findPreference("quick_settings_fragment").setLayoutResource(R.layout.top_level_preference_bottom);
+        findPreference("lockscreen_fragment").setLayoutResource(R.layout.top_level_preference_alone);
         findPreference("btn_fragment").setLayoutResource(R.layout.top_level_preference_top);
         findPreference("notifications_fragment").setLayoutResource(R.layout.top_level_preference_bottom);
         findPreference("misc_fragment").setLayoutResource(R.layout.top_level_preference_alone);
