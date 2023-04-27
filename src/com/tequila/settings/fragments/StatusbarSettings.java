1918e506e664831cbb909e5a0a16183a417e5277
minaripenguin37 <alexfinhart@gmail.com>
2023-04-27 04:28:58 +0000

    tequilaSettings: Combined signal icons toggle [2/2]
    Change-Id: I64bd59726cd3431733896dc6e96f8957be6a7339

--- src/com/tequila/settings/fragments/StatusbarSettings.java
+++ src/com/tequila/settings/fragments/StatusbarSettings.java
@@ -1,7 +1,9 @@
 package com.tequila.settings.fragments;
 
-import android.os.Bundle;
 import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
@@ -14,6 +16,8 @@ import com.android.settings.SettingsPreferenceFragment;
 public class StatusbarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private Preference mCombinedSignalIcons;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -21,12 +25,20 @@ public class StatusbarSettings extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
+
+        mCombinedSignalIcons = findPreference("persist.sys.flags.combined_signal_icons");
+        mCombinedSignalIcons.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final ContentResolver resolver = getActivity().getContentResolver();
-
+        if (preference == mCombinedSignalIcons) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putIntForUser(getContentResolver(),
+                Settings.Secure.ENABLE_COMBINED_SIGNAL_ICONS, value ? 1 : 0, UserHandle.USER_CURRENT);
+            return true;
+        }
         return false;
     }
 
